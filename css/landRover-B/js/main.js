294da3cd52f879f96328b46b17917f611389ebd2
dcongfang <751928511@qq.com>
2019-04-11 21:42:07 +0800

    new file

---
+++ css/landRover-B/js/main.js
@@ -0,0 +1,10 @@
+(function() {
+    $('.ui.navbar .search-icon i').click(function() {
+        const icon = $(this);
+        if(icon.text() === 'search'){
+        icon.text('close').parents('.content').addClass('searching')
+        }else {
+            icon.text('search').parents('.content').removeClass('searching')
+        }
+    })
+})();
