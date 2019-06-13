d8b813d804e05a60b2f4b23a13378fd887638232
dcongfang <751928511@qq.com>
2019-06-13 21:40:08 +0800

    eventEmit学习

--- VUE/vue-eleme-app/src/components/goods/goods.vue
+++ VUE/vue-eleme-app/src/components/goods/goods.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="goods">
-      <div class="menu-wrappper" ref="menuWrapper">
+      <div class="menu-wrapper" ref="menuWrapper">
         <ul>
           <li 
           v-for="(item, index) in goods" 
@@ -17,11 +17,42 @@
           </li>
         </ul>
       </div>
+      <div class="foods-wrapper" ref="foods">
+        <ul>
+          <li v-for="(item, index) in goods" :key="index" class="food-list" ref="foodList">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="(food, index) in item.foods" :key="index" class="food-item border-1px">
+                <div class="icon">
+                  <img :src="food.icon" width="57" height="57" alt="">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                  <!-- 添加 -->
+                  <div class="cartcontrol-wrapper">
+                    <cartcontrol :food="food" @add="addFood"></cartcontrol>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import cartcontrol from '../cartcontrol/cartcontrol'
 import BScorll from 'better-scroll'
 export default {
   name: 'Goods',
@@ -31,11 +62,23 @@ export default {
       goods: []
     }
   },
+  components: {
+    cartcontrol
+  },
   methods: {
     _initScroll () { // 私有方法
     this.menuScroll = new BScorll(this.$refs.menuWrapper, {
       click: true
     })
+    },
+    addFood(target) {
+      this._drop(target)
+    },
+    _drop (target) {
+      //体验优化,异步执行下落动画
+      this.$nextTick(() => {
+        // 动画组件
+      })
     }
   },
   created() {
@@ -63,7 +106,7 @@ export default {
   bottom 46px
   width 100%
   overflow hidden
-  .menu-wrappper
+  .menu-wrapper
     flex 0 0 80px
     width 80px
     background #f3f5f7
@@ -81,6 +124,11 @@ export default {
         font-weight 700
       .text
         border-none()
+        display table-cell
+        width 56px
+        vertical-align middle
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size 12px
         .icon
           display inline-block
           vertical-align top
@@ -104,4 +152,59 @@ export default {
           &.special {
             bg-image('special_3');
           }
+  .foods-wrapper
+    flex 1
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147, 153, 159)
+      background #f3f5f7
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        border-none()
+        margin-bottom 0
+
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+      .content
+        flex 1
+        .name
+          margin 2px 0 8px 0
+          height 14px
+          line-height 14px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .desc,.extra
+          line-height 10px
+          font-size 10px
+          color rgb(147, 153, 159)
+        .desc
+          line-height 12px
+          margin-bottom 8px
+        .extra
+          .count
+            margin-right 12px
+        .price
+          font-weight 700
+          line-height 24px
+          .now
+            margin-right 8px
+            font-size 8px
+            color rgb(240, 20, 20)
+          .old
+            text-direction line-through
+            font-size 10px
+            color rgb(147, 153, 159)
+        .cartcontrol-wrapper
+          position absolute
+          right 0
+          bottom 12px
 </style>
