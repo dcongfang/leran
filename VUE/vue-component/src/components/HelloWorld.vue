b0f4bcc37298f8269fe0534f13008e4a9933f6a3
dcongfang <751928511@qq.com>
2019-07-09 18:19:27 +0800

    taro学习

---
+++ VUE/vue-component/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <!-- <button @click="submit">登录</button> -->
+    <i-button size="small">按钮</i-button>
+    <i-menu />
+  </div>
+</template>
+
+<script>
+import Button from './com/Button'
+import Menu from './com/Menu'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    'i-button': Button,
+    'i-menu': Menu
+  },
+  methods: {
+    submit() {
+      this.$toast('登录成功', 'top')
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
