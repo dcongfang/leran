c55b4bbbc360503683e5d28fa78f217d0b040f6f
dcongfang <751928511@qq.com>
2019-07-10 11:53:03 +0800

    面试题

---
+++ VUE/vue-component/src/components/com/display.vue
@@ -0,0 +1,18 @@
+<template>
+  <div ref="display"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    code: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/vue-component/src/view/Display.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>动态渲染 .vue 文件的组件--display</h3>
+    <i-display :code="code"></i-display>
+  </div>
+</template>
+
+<script>
+import defaultCode from './default-code'
+import iDisplay from '@/components/com/display'
+export default {
+  components: { iDisplay },
+  data () {
+    return {
+      code: defaultCode
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
