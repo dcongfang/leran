852919f76f1ef7ab45196034f9b9eadcd944ad19
dcongfang <751928511@qq.com>
2019-07-11 21:29:38 +0800

    react实战

---
+++ react/hammer-market/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ShopIndex from './components/shopIndex/ShopIndex'
+
+function App() {
+  return (
+    <div className="App">
+      <ShopIndex></ShopIndex>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/hammer-market/src/components/shopIndex/ShopIndex.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+class ShopIndex extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+      <div className="header">
+        <div className="icon-font">
+          <img src="../../assets/images/sort.png" alt=""/>
+        </div>
+        <div className="icon-font1">
+          <img src="../../assets/images/chuiz.png" alt=""/>
+        </div>
+        <div className="icon-font2">
+          <img src="../../assets/images/search.png" alt=""/>
+        </div>
+      </div>
+      </div>
+     );
+  }
+}
+ 
+export default ShopIndex;
