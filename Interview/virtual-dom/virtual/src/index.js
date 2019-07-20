37738ddc504d75461971decf4c483ab7b8134376
dcongfang <751928511@qq.com>
2019-07-20 22:21:07 +0800

    diff

---
+++ Interview/virtual-dom/virtual/src/index.js
@@ -0,0 +1,10 @@
+import {createElement, render, renderDOM}  from './element'
+// ul.list>li.item*3
+
+let virtualDOM = createElement('ul', {class:'list'}, [
+  createElement('li', {class: 'item'}, ['周杰伦']),
+  createElement('li', {class: 'item'}, ['王力宏']),
+  createElement('li', {class: 'item'}, ['林俊杰']),
+]);
+renderDOM(render(virtualDOM), document.getElementById('root'))
+console.log(virtualDOM)
---
+++ node/make-koa/onion.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const app = new Koa();
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log(2)
+})
+app.use(async (ctx) => {
+  
+})
