1755ec16c8c2d15549002e8d3cee5ced3ac2a1cf
dcongfang <751928511@qq.com>
2019-07-29 21:43:41 +0800

    react-router

--- react/make-react-router/src/react-router-dom/BrowserRouter.jsx
+++ react/make-react-router/src/react-router-dom/BrowserRouter.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Router } from '../react-router/index';
-import { createBrowserHistory as createHistory } from 'history'
+import React from "react";
+import { Router } from "../react-router/index";
+import { createBrowserHistory as createHistory } from "history";
+
+/**
+ * The public API for a <Router> that uses HTML5 history.
+ */
 class BrowserRouter extends React.Component {
-  history = createHistory(this.props)
+  // HTML5 history API 
+  history = createHistory(this.props);
+  /**
+   * {
+    * length: stack length,
+    * location: window.location,
+    * action: 当前导航的动作
+    * listen： 监听变化，
+    * 导航相关的
+    * go: 
+    * push: 
+    * Blocking：确认离开？
+   * }
+   */
+  
   render() {
-    console.log(this.history)
-    return (
-      <Router history={this.history} children={this.props.children}></Router>
-    )
+    return <Router history={this.history} children={this.props.children} />;
   }
 }
-export default BrowserRouter
+
+export default BrowserRouter;
