b37f2aa27b19cbb05818507e167d1a28529240d4
dcongfang <751928511@qq.com>
2019-08-05 23:03:28 +0800

    排序

--- js/com/5.js
+++ js/com/5.js
@@ -17,19 +17,19 @@ console.log(select([2,1,9,6,4,5,3,7]))
 // 优化: 冒泡有一个最大的问题就是这种算法不管不管你有序还是没序，闭着眼睛把你循环比较了再说。
 // 优化方案  在操作之前判断目标数组是否已经是排序好了的
 
-function bubble(arr) {
-  for(let i = 0; i < arr.length; i++) {
-    for(let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1])
-      {
-        let temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
-      }
-    }
-  }
-  return arr
-}
+// function bubble(arr) {
+//   for(let i = 0; i < arr.length; i++) {
+//     for(let j = 0; j < arr.length - i - 1; j++) {
+//       if (arr[j] > arr[j + 1])
+//       {
+//         let temp = arr[j]
+//         arr[j] = arr[j + 1]
+//         arr[j + 1] = temp
+//       }
+//     }
+//   }
+//   return arr
+// }
 
-console.log(bubble([2,1,9,6,4,5,3,7]))
+// console.log(bubble([2,1,9,6,4,5,3,7]))
 
